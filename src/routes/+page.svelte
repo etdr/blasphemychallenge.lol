14dc8074154829982761eb0707ede809bc754d3f
Eli T. Drumm <eli@eli.td>
2024-08-25 19:06:59 -0400

    starting to write text and add stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<h1>Blasphemy Challenge</h1>
+
+<a href="/about">about</a>
+
+
+
+<blockquote>
+	<sup>31</sup>And so I tell you, every kind of sin and slander can be forgiven, but blasphemy against the Spirit will not be forgiven. <sup>32</sup>Anyone who speaks a word against the Son of Man will be forgiven, but anyone who speaks against the Holy Spirit will not be forgiven, either in this age or in the age to come.
+	<p><cite>Matthew 12:31-32 (New International Version)</cite></p>
+</blockquote>
+
+
+
+
+
+
+
+<form>
+
+</form>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+
+<a href="/">home</a>
+
+<h2>about</h2>
+
+
+<p><strong>Have you ever wanted to absolutely ensure you will never enter the Kingdom of Heaven?</strong></p>
+
+<p>This seems like a straightforward task. Surely you just have to do something sufficiently evil? Yes, this might be hard work and time-consuming, but you would think it would get the job done, right?</p>
+
+
+<p>The problem is, Jesus (and by extension the New Testament Abrahamic God) are <em>very</em> forgiving. In many modern Christian traditions, no matter what horrifying atrocities you commit, you can come to Christ and ask for forgiveness and Saint Robert's your uncle, you are good to go.</p>
+
+<p>Enter Matthew 12. </p>
